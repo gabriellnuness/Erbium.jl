1cbb6b215c3b0ad7ee1f5e6d79e72128f5dd54c4
Gabriel Nunes <40604481+gabriellnuness@users.noreply.github.com>
2023-06-26 14:08:49 -0300

    data: comma sep files

--- src/optical_fiber.jl
+++ src/optical_fiber.jl
@@ -12,33 +12,36 @@ function optical_fiber(fiber::String)
     if fiber == "m5"
         
         # β = σ⋅NT
-        β_abs = readdlm("..\\data\\M5_abs.txt",'\t')
-        β_emis = readdlm("..\\data\\M5_emis.txt",'\t')
+        β_abs = readdlm("data\\M5_abs.txt",',')
+        β_emis = readdlm("data\\M5_emis.txt",',')
         
         diameter = (5.7+6.6) / 2
-        sigma_p_a = 
-        sigma_p_e = 
-        sigma_a = 
-        sigma_e = 
-        N2 = 
-        NT = 
+        # sigma_p_a = 
+        # sigma_p_e = 
+        # sigma_a = 
+        # sigma_e = 
+        # N2 = 
+        # NT = 
         τ21 = 1e-6
         τ3 = 10e-3
+
+        (β_abs, β_emis, τ21, τ3, diameter)
 
     elseif fiber == "m12"
     
         diameter = (5.5+6.3) / 2
-        sigma_p_a = 
-        sigma_p_e = 
-        N2 = 
-        NT = 
-        sigma_a = 
-        sigma_e = 
+        # sigma_p_a = 
+        # sigma_p_e = 
+        # N2 = 
+        # NT = 
+        # sigma_a = 
+        # sigma_e = 
         τ21 = 1e-6
         τ3 = 10e-3
-        β_abs = readdlm("..\\data\\M12_abs.txt",'\t')
-        β_emis = readdlm("..\\data\\M12_emis.txt",'\t')
+        β_abs = readdlm("data\\M12_abs.txt",',')
+        β_emis = readdlm("data\\M12_emis.txt",',')
     
+        (β_abs, β_emis, τ21, τ3, diameter)
     end
 
 end
